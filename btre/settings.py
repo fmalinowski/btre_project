04075e163a8a075dfe27640f903c63ce9a500378
Francois Malinowski <francois@francois-malinowski.fr>
2019-08-18 18:27:08 -0600

    Add send email functionality for inquiries

--- btre/settings.py
+++ btre/settings.py
@@ -141,3 +141,10 @@ MEDIA_URL = '/media/'
 MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
+
+# Email config
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'gmail user'
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = True
